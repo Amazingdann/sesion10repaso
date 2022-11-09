ef4045cbce3ffde24e7f29d3916cea2ec6993dc8
Amazingdann <alvarezvaldaniel@hotmail.com>
2022-11-08 22:42:14 -0600

    sesión de repaso settimeout & set intervalnterv

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 console.log("Entro al main.js");
+const body= document.querySelector("body");
+
+function btnCambiarColor(){
+    console.log("entró a cambiar color");
+    setTimeout(cambiarColor, 1000)
+    console.log("debería ejecutar al Final el cambio de color");
+    //cambiarcolor();   
+    //setInterval(cambiarColor, 1000)
+}
+function cambiarColor(){
+    //console.log("estácambiaando el color")
+    const randomcolor = Math.floor(Math.random()*16777215).toString(16);
+   // console.log("al color:" + randomcolor)
+    body.style.backgroundColor = "#"+randomcolor;
+}
